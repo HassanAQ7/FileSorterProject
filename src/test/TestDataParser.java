ecfe86dae233117f1f248d66b292bdd622dad13e
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-04-30 22:22:21 -0700

    Updated Tests to reflect changes in testFile location and implementation changes

--- src/test/TestDataParser.java
+++ src/test/TestDataParser.java
@@ -3,6 +3,9 @@ package test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.*;
+
 import model.DataRecord;
 import ui.DataParser;
 
@@ -17,17 +20,19 @@ public class TestDataParser {
 
     @Test
     public void testNoCSVFile() {
-        DataParser csvFile = new DataParser("src/nonexistent.csv");
+        DataParser csvFile = new DataParser("resources/nonexistent.csv");
         try {
             csvFile.parseRecords();
         } catch (IOException e) {
             // pass
+        } catch (CsvException e) {
+            fail();
         }
     }
 
     @Test
     public void testExistentCSVFile() {
-        DataParser csvFile = new DataParser("src\\testFile.csv");
+        DataParser csvFile = new DataParser("./resources/testFile.csv");
         try {
             List<DataRecord> records = csvFile.parseRecords();
             assertEquals(2, records.size());
@@ -47,6 +52,8 @@ public class TestDataParser {
 
         } catch (IOException e) {
             fail("Exception shouldn't have been thrown, file exists");
+        } catch (CsvException e) {
+            fail("No need for exception to be thrown");
         }
     }
 
