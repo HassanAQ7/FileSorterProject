c25e311d82d429ca91fc5245e1d58e8a84f189d1
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-05-03 14:19:26 -0700

    Extended functionality of DataParser to skip rows in CSVFile

--- src/ui/DataParser.java
+++ src/ui/DataParser.java
@@ -1,37 +1,47 @@
 package ui;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import model.DataRecord;
 import com.opencsv.*;
 import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
+
+import exceptions.FileSkipException;
 
 // Represents a DataParser that can get records from a given path
 public class DataParser {
     private String path;
+    private boolean skip;
 
     // EFFECTS: Constructs a DataParser with provided path
-    public DataParser(String path) {
+    // and whether it must skip lines or not
+    public DataParser(String path, boolean skip) {
         this.path = path;
+        this.skip = skip;
     }
-
 
     // EFFECTS: Parses DataRecords and fields from CSV File and returns it
     // Throws IOException if file cannot be found
     // or CsvException if other issues with reading file arises
-    public List<DataRecord> parseRecords() throws IOException, CsvException {
-        List<DataRecord> records = new LinkedList<>();
+    public List<DataRecord> parseRecords(int skipLines) throws IOException, CsvException, FileSkipException {
+        if (!skip && skipLines > 0) {
+            throw new FileSkipException();
+        }
+        
+        List<DataRecord> records = new ArrayList<>();
         FileReader reader = new FileReader(path);
 
-        CSVReader recordReader = new CSVReader(reader);
+        CSVReader recordReader;
+        if (skip) {
+            recordReader = new CSVReaderBuilder(reader).withSkipLines(skipLines).build();
+        } else {
+            recordReader = new CSVReader(reader);
+        }
         String[] headers = recordReader.readNext();
         String[] recordValues;
 
@@ -43,6 +53,7 @@ public class DataParser {
             records.add(new DataRecord(columnValues));
         }
 
+        recordReader.close();
         return records;
 
     }
