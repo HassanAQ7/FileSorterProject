93691e6411998c08d2d50f35a5673328ee8d10cd
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-05-06 01:28:07 -0700

    Provided specifications for RecordSorter class

---
+++ src/model/RecordSorter.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.List;
+
+// Represents the system that can sort records in terms
+// of some specified column and algorithm
+public class RecordSorter {
+
+    // EFFECTS: Constructs a new RecordSorter with provided
+    // list of records to sort
+    public RecordSorter(List<DataRecord> records) {
+
+    }
+
+    public List<DataRecord> getRecords() {
+        return null; // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Sorts the provided list of records based on a category and ComparisonType
+    public void sortRecords(String category, RecordComparator.CompareType type) {
+    
+    }
+
+
+
+}
