8777ef9c13a9ded76456061871756b50d5b6b77f
HassanAQ7 <hassan.allyqureshi@gmail.com>
2025-05-05 23:50:57 -0700

    Provided implementation for RecordComparator class

--- src/model/RecordComparator.java
+++ src/model/RecordComparator.java
@@ -1,7 +1,9 @@
 package model;
 
-import java.util.List;
-import java.util.ArrayList;
+
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.text.ParseException;
 import java.util.Comparator;
 
 // Represents a Comparator that compares different DataRecords
@@ -11,19 +13,49 @@ public class RecordComparator implements Comparator<DataRecord> {
         NUMERIC, ALPHABETICAL, DATE
     }
 
+    private String category;
+    private CompareType type;
+
     // EFFECTS: Creates a new Comparator with provided category to sort by
     // and provided comparison strategy
     public RecordComparator(String category, CompareType type) {
+        this.category = category;
+        this.type = type;
+    }
 
+    public String getCategory() {
+        return category;
+    }
+
+    public CompareType getCompareType() {
+        return type;
+    }
+
+    public void setCompareType(CompareType type) {
+        this.type = type;
     }
 
     public void setSortCategory(String category) {
-
+        this.category = category;
     }
 
     @Override
     public int compare(DataRecord o1, DataRecord o2) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'compare'");
+        String stringVal1 = o1.getColumnValues().get(category);
+        String stringVal2 = o2.getColumnValues().get(category);
+        if (type == CompareType.NUMERIC) {
+            return Double.compare(Double.parseDouble(stringVal1), Double.parseDouble(stringVal2));
+        } else if (type == CompareType.ALPHABETICAL) {
+            return stringVal1.compareTo(stringVal2);
+        } else {
+            String[] dateFormats = { "yyyy/MM/dd", "dd/MM/yyyy", "yyyy-MM-dd" };
+            try {
+                return DateUtils.parseDateStrictly(stringVal1, dateFormats).compareTo(
+                        DateUtils.parseDateStrictly(stringVal2, dateFormats));
+            } catch (ParseException e) {
+                throw new IllegalArgumentException("Wrong Date format");
+            }
+        }
+
     }
 }
